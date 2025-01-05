8f450d92a510501ad20a48b758fa457f8d2b84a4
swz-git <contact@swz.works>
2025-01-05 02:22:02 +0100

    support $RLBOT_SERVER_IP

--- crates/rlbot/src/util.rs
+++ crates/rlbot/src/util.rs
@@ -1,17 +1,18 @@
 use std::env;
 
 pub struct RLBotEnvironment {
-    /// Will fallback to 126.0.0.1:23234
+    /// Will fallback to 127.0.0.1:23234
     pub server_addr: String,
     /// No fallback and therefor Option<>
     pub agent_id: Option<String>,
 }
 
 impl RLBotEnvironment {
-    // Reads from environment variables RLBOT_SERVER_ADDR/RLBOT_SERVER_PORT and RLBOT_AGENT_ID
+    // Reads from environment variables RLBOT_SERVER_ADDR/(RLBOT_SERVER_IP & RLBOT_SERVER_PORT) and RLBOT_AGENT_ID
     pub fn from_env() -> Self {
         let server_addr = env::var("RLBOT_SERVER_ADDR").unwrap_or(format!(
-            "127.0.0.1:{}",
+            "{}:{}",
+            env::var("RLBOT_SERVER_IP").unwrap_or("127.0.0.1".into()),
             env::var("RLBOT_SERVER_PORT").unwrap_or("23234".into())
         ));
         let mut agent_id = env::var("RLBOT_AGENT_ID").ok();
