0854607f50402abe3eb8f46917ad41e9d8eead17
swz-git <contact@swz.works>
2025-04-03 21:31:28 +0200

    use custom branch of planus for in-memory translation and better build speed

--- rlbot_flat/build.rs
+++ rlbot_flat/build.rs
@@ -7,7 +7,6 @@ use std::{
 };
 
 const SCHEMA_DIR: &str = "../../flatbuffers-schema";
-const SCHEMA_DIR_TEMP: &str = "./flatbuffers-schemaTEMP";
 const OUT_FILE: &str = "./src/planus_flat.rs";
 
 // this is pretty janky, but it works
@@ -37,35 +36,34 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|x| format!("include \"{x}\";"))
         .collect::<String>();
 
-    let temp_file_paths: Vec<PathBuf> = fbs_file_names
+    let files = fbs_file_paths
         .iter()
-        .map(|x| PathBuf::from(SCHEMA_DIR_TEMP).join(x))
-        .collect();
+        .map(|fbs_file_path| {
+            let mut contents = fs::read_to_string(fbs_file_path).expect("failed to read file");
 
-    if !fs::exists(SCHEMA_DIR_TEMP)? {
-        fs::create_dir(SCHEMA_DIR_TEMP)?;
-    }
+            // planus doesn't support multiple root_types
+            // removing them doesn't seem to do much
+            contents = contents.replace("root_type", "// root_type");
 
-    for (fbs_file_path, temp_file_path) in fbs_file_paths.iter().zip(temp_file_paths.iter()) {
-        let mut contents = fs::read_to_string(fbs_file_path)?;
+            // comment all existing includes
+            contents = contents.replace("include \"", "// include \"");
 
-        // planus doesn't support multiple root_types
-        // removing them doesn't seem to do much
-        contents = contents.replace("root_type", "// root_type");
+            // include all files (since we're removing root_types the root_types aren't auto-included)
+            contents = include_all_str.clone() + &contents;
 
-        // comment all existing includes
-        contents = contents.replace("include \"", "// include \"");
-
-        // include all files (since we're removing root_types the root_types aren't auto-included)
-        contents = include_all_str.clone() + &contents;
-
-        fs::File::create(temp_file_path)?.write_all(contents.as_bytes())?;
-    }
+            (
+                fbs_file_path
+                    .strip_prefix(SCHEMA_DIR)
+                    .expect("failed to strip SCHEMA_DIR prefix from file path"),
+                contents,
+            )
+        })
+        .collect::<Vec<_>>();
 
     let start_time_planus = Instant::now();
 
     let declarations =
-        planus_translation::translate_files(&temp_file_paths).ok_or("planus translation failed")?;
+        planus_translation::translate_files_from_memory_with_options(&files, Default::default());
     let mut res = planus_codegen::generate_rust(&declarations)?;
 
     // No idea why planus renames RLBot to RlBot but this fixes it
@@ -82,8 +80,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
 
     fs::File::create(OUT_FILE)?.write_all(&[time_taken.as_bytes(), res.as_bytes()].concat())?;
-
-    fs::remove_dir_all(SCHEMA_DIR_TEMP)?;
 
     Ok(())
 }
